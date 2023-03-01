451c7b38876cb931140be02da148f99ceb2e70f9
Deepak <deepakkosuri92@gmail.com>
2023-03-01 14:41:06 +0530

    Added ScrollConfig and ScrollBar

--- lib/dashboard/view/dashboard_view.dart
+++ lib/dashboard/view/dashboard_view.dart
@@ -1,4 +1,5 @@
 //? Dashboard View
+import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:my_web_demo/common/styles/app_colors.dart';
@@ -14,11 +15,13 @@ class DashboardView extends StatefulWidget {
 
 class _DashboardViewState extends State<DashboardView> {
   late PageController pageController;
+  late ScrollController scrollController;
 
   @override
   void initState() {
     super.initState();
     pageController = PageController();
+    scrollController = ScrollController();
   }
 
   @override
@@ -29,13 +32,26 @@ class _DashboardViewState extends State<DashboardView> {
         backgroundColor: AppColors.kSecondaryColor,
         title: const Text(AppStrings.dashboard),
       ),
-      body: PageView(
-        controller: pageController,
-        scrollDirection: Axis.vertical,
-        children: [
-          _firstPage(context, color: Colors.yellow, text: 'Page 1'),
-          _firstPage(context, color: Colors.orange, text: 'Page 2'),
-        ],
+      body: Scrollbar(
+        isAlwaysShown: true,
+        interactive: true,
+        hoverThickness: 10,
+        trackVisibility: true,
+        controller: scrollController,
+        child: ScrollConfiguration(
+          behavior: ScrollConfiguration.of(context).copyWith(dragDevices: {
+            PointerDeviceKind.touch,
+            PointerDeviceKind.mouse,
+          }),
+          child: PageView(
+            controller: pageController,
+            scrollDirection: Axis.vertical,
+            children: [
+              _firstPage(context, color: Colors.yellow, text: 'Page 1'),
+              _firstPage(context, color: Colors.orange, text: 'Page 2'),
+            ],
+          ),
+        ),
       ),
     );
   }
@@ -81,6 +97,18 @@ class DashDrawer extends StatelessWidget {
                       .setGFont(style: GoogleFonts.aBeeZee()),
                 ),
               ),
+            ),
+            ListTile(
+              title: const Text('Page 1'),
+              onTap: () => pageController.animateToPage(0,
+                  curve: Curves.easeIn,
+                  duration: const Duration(milliseconds: 800)),
+            ),
+            ListTile(
+              title: const Text('Page 2'),
+              onTap: () => pageController.animateToPage(1,
+                  curve: Curves.easeIn,
+                  duration: const Duration(milliseconds: 800)),
             ),
           ],
         ),
